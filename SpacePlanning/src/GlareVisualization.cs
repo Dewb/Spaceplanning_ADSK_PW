1e1157dd1a27a3010793c4d51609ea11448f88d7
Subhajit Das <t_dassu@ads.autodesk.com>
2016-05-18 16:24:51 -0400

    Checkpt: Results

--- SpacePlanning/src/GlareVisualization.cs
+++ SpacePlanning/src/GlareVisualization.cs
@@ -68,8 +68,9 @@ namespace SpacePlanning
 
 
         //gets the mid point of the furnitures in point3d
-        public static List<Point3d> GetLightingPoints(List<Cell> cellList, double height = 0)
+        public static List<Point3d> GetLightingPointsRandomly(List<Cell> cellList, Random ran, double height = 0, double maxNum = 20, double recompute = 5)
         {
+            double count = 0;
             List<Point3d> ptList = new List<Point3d>(), lightPtList = new List<Point3d>();
             for (int i = 0; i < cellList.Count; i++)
             {
@@ -77,18 +78,22 @@ namespace SpacePlanning
                 ptList.Add(pt3d);
             }
           
-            Random ran = new Random();
+            //Random ran = new Random();
             for(int i = 0; i < ptList.Count; i++)
             {
-                double num = BasicUtility.RandomBetweenNumbers(ran, 0, 5);
-                if (num < 3) lightPtList.Add(ptList[i]);
+                count += 1;
+                if(count < maxNum)
+                {
+                    double num = BasicUtility.RandomBetweenNumbers(ran, 0, 5);
+                    if (num < 3) lightPtList.Add(ptList[i]);
+                }               
             }
             return lightPtList;
         }
 
 
         //gets the mid point of the furnitures in point3d
-        public static List<Point3d> GetLightingPointsNAmount(List<Cell> cellList, double height = 0, double num = 15)
+        public static List<Point3d> GetLightingPointsNAmount(List<Cell> cellList, double height = 0, double startPt = 0, double num = 15)
         {
             List<Point3d> ptList = new List<Point3d>(), lightPtList = new List<Point3d>();
             for (int i = 0; i < cellList.Count; i++)
@@ -96,11 +101,21 @@ namespace SpacePlanning
                 Point3d pt3d = ConvertToPoint3d(cellList[i].CenterPoint, height);
                 ptList.Add(pt3d);
             }
-            Random ran = new Random();
-            for (int i = 0; i < ptList.Count; i++) if (i < num) lightPtList.Add(ptList[i]);
+            for (int i = 0; i < ptList.Count; i++) if (i > startPt && i < num+startPt) lightPtList.Add(ptList[i]);
+            return lightPtList;
+        }
+
+
+        //gets the mid point of the furnitures in point3d
+        public static List<Point3d> GetLightingPointsEveryXTime(List<Cell> cellList, double height = 0, double div = 15)
+        {
+            List<Point3d> ptList = new List<Point3d>(), lightPtList = new List<Point3d>();
+            for (int i = 0; i < cellList.Count; i++)
             {
-               
+                Point3d pt3d = ConvertToPoint3d(cellList[i].CenterPoint, height);
+                ptList.Add(pt3d);
             }
+            for (int i = 0; i < ptList.Count; i++) if (i % div == 0) lightPtList.Add(ptList[i]);
             return lightPtList;
         }
 
@@ -139,22 +154,27 @@ namespace SpacePlanning
 
 
         //computes the UGR value
-        public static double ComputeUGRValue(List<Point3d> lightPts, Point3d observer, double lightSize)
+        public static double ComputeUGRValue(List<Point3d> lightPts, Point3d observer, double lightSize=2, int pos = 0, double recompute = 1)
         {
             double backLum = 10;
-            double lumin = 15;
+            double lumin = 10, inc = 100;
             double angleEach = 0;
-            double guthPos = 2*lightSize;
+            double guthPos = 2;
             double value = 0;
             double summation = 0;
             
             for(int i = 0; i < lightPts.Count; i++)
-            {
+            {                
+                if (i == pos) lumin = recompute*lumin;
+                else lumin = 10;
+                //if (i < lightPts.Count / 2) lumin += inc;
+                //else lumin -= inc;
                 if (lightPts[i].X > observer.X)
                 {
                     Point2d ptLight2d = ConvertToPoint2d(lightPts[i]);
                     Point2d ptObserver2d = ConvertToPoint2d(observer);
                     double distance = GraphicsUtility.DistanceBetweenPoints(ptLight2d, ptObserver2d);
+                    guthPos = distance;
                     
                     Point3d lightPt1 = new Point3d((lightPts[i].X - lightSize), (lightPts[i].Y - lightSize), lightPts[i].Z);
                     Point3d lightPt2 = new Point3d((lightPts[i].X + lightSize), (lightPts[i].Y + lightSize), lightPts[i].Z);
@@ -167,14 +187,16 @@ namespace SpacePlanning
             value = (0.25 / backLum) * summation;
             if (value == 0) return 0;
             double ugr = 8 * Math.Log10(value);
+            if (ugr < 0) ugr = 0;
             return ugr;
         }
 
 
         //compute glare angle values
-        public static List<List<double>> ComputerGlareValues(List<Point3d> floorPoints,List<Point3d> furniturePoints,
-            List<Point3d> lightPoints, double threshDist = 10, double thresAngleLow = 50, double thresAngleHigh = 80, double lightSize = 3)
+        public static List<List<double>> ComputeGlareValues(List<Point3d> floorPoints,List<Point3d> furniturePoints,
+            List<Point3d> lightPoints, double threshDist = 10, double thresAngleLow = 50, double thresAngleHigh = 80, double lightSize = 3, double recompute =1)
         {
+            int pos = 0;
             List<double> angList = new List<double>();
             List<double> ugrList = new List<double>();
             int count = 0;
@@ -188,9 +210,9 @@ namespace SpacePlanning
                     double distance = GraphicsUtility.DistanceBetweenPoints(pt2Furniture, pt2FloorPt);
                     if (distance < threshDist) selectedPts.Add(furniturePoints[j]);
                 }// end of j for loop               
-
-                if (selectedPts.Count > 0) lightPoints.AddRange(selectedPts);
-                double ugrValue = ComputeUGRValue(lightPoints, floorPoints[i], lightSize);
+                pos = (int)(lightPoints.Count * 0.80);
+                if (selectedPts.Count > 0) lightPoints.AddRange(selectedPts);               
+                double ugrValue = ComputeUGRValue(lightPoints, floorPoints[i], lightSize, pos, recompute);
                 ugrList.Add(ugrValue);
                 count += 1;
             }
@@ -204,87 +226,13 @@ namespace SpacePlanning
             result.Add(angList);
             result.Add(new List<double>{count});
             result.Add(ugrList);
+            result.Add(new List<double> { lightSize });
+            result.Add(new List<double> { pos });
             return result;          
         }
 
 
-        //compute glare angle values
-        public static List<List<double>> ComputerGlareValuesOld(List<Point3d> floorPoints, List<Point3d> furniturePoints,
-            List<Point3d> lightPoints, double threshDist = 10, double thresAngleLow = 50, double thresAngleHigh = 80)
-        {
-            List<double> angList = new List<double>();
-            List<double> allAngles = new List<double>();
-            int count = 0;
-            for (int i = 0; i < floorPoints.Count; i++)
-            {
-                List<Point3d> selectedPts = new List<Point3d>();
-                Point2d pt2FloorPt = ConvertToPoint2d(floorPoints[i]);
-                for (int j = 0; j < furniturePoints.Count; j++)
-                {
-                    Point2d pt2Furniture = ConvertToPoint2d(furniturePoints[j]);
-                    double distance = GraphicsUtility.DistanceBetweenPoints(pt2Furniture, pt2FloorPt);
-                    if (distance < threshDist) selectedPts.Add(furniturePoints[j]);
-                }// end of j for loop
-                double ang = 0;
-                for (int k = 0; k < selectedPts.Count; k++)
-                {
-                    //ang = 0;
-                    List<Vector3d> vec3dList = new List<Vector3d>();
-                    for (int m = 0; m < lightPoints.Count; m++)
-                    {
-                        if (lightPoints[m].X < selectedPts[k].X)
-                        {
-                            Vector3d vx = new Vector3d(1, 0, 0);
-                            Vector3d vy = new Vector3d(0, 1, 0);
-                            Vector3d vz = new Vector3d(0, 0, 1);
-
-                            Vector3d vec = new Vector3d(lightPoints[m], selectedPts[k]);
-                            vec3dList.Add(vec);
-                            double angle = VectorUtility.AngleBetween(vec, vx);
-                            Trace.WriteLine("Angle value is : " + angle);
-                            if (angle > thresAngleLow && angle < thresAngleHigh) ang += angle;
-                            else { angle = 0; }
-                            //ang += angle;
-                            allAngles.Add(angle);
-                        }
-                        count += 1;
-                    }
-                }
-                if (ang == 0) angList.Add(-1);
-                else angList.Add((ang / selectedPts.Count));
-
-
-            }// end of i for loop
-            //return angList;
-
-            List<int> indexList = new List<int>();
-            for (int n = 0; n < angList.Count; n++) indexList.Add(n);
-
-            int colorVal = 255;
-            List<int> sortedIndices = BasicUtility.Quicksort(angList, indexList, 0, angList.Count - 1);
-            sortedIndices.Reverse();
-            double maxAng = angList[sortedIndices[0]];
-            List<double> angListNormalized = new List<double>();
-            List<double> val2 = new List<double>();
-            List<double> val3 = new List<double>();
-            for (int n = 0; n < angList.Count; n++)
-            {
-                angListNormalized.Add(angList[n] * colorVal / maxAng);
-                val2.Add(0);
-                val3.Add(0);
-            }
-            List<List<double>> result = new List<List<double>>();
-            result.Add(angListNormalized);
-            result.Add(val2);
-            result.Add(val3);
-            result.Add(angList);
-            result.Add(new List<double> { count });
-            result.Add(allAngles);
-            return result;
-
-
-        }
-
+      
 
 
     }
